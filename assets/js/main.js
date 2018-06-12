735d11aaf57cf63994cdb4857dfd550cb6d768f8
igor Cuevas <igor.cuevas@hsrtech.local>
2018-06-12 15:58:41 -0300

    Update CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,7 +123,21 @@ function searchNav() {
 			li[i].style.display = "";
 		} else {
 			li[i].style.display = "none";
-
 		}
 	}
+}
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
 }
